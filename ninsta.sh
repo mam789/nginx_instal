2ab0b2acab04a373119d5cdeac8ffa9ccc9da6b3
sanchi <126137666+sanchi789@users.noreply.github.com>
2023-03-10 09:49:53 +0530

    Update ninsta.sh

--- ninsta.sh
+++ ninsta.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
+
 # Check if the user is running the script with root privileges
+
+
 if [ "$EUID" -ne 0 ]
   then echo "Please run this script with root privileges."
   exit
 fi
 
 # Detect the operating system
+
 if [ "$(uname)" == "Darwin" ]; then
-  # Mac OS X platform
+  # for Mac OS X platform
   brew install nginx
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+elif  [ "$(uname -s)" == "Linux" ]; then
   # Linux platform
   if [ -f /etc/lsb-release ]; then
-    # Ubuntu or Debian
+    # Ubuntu Debian
     apt-get update
     apt-get install nginx
   elif [ -f /etc/redhat-release ]; then
-    # Red Hat or CentOS
+    # for Red Hat
     yum update
     yum install nginx
   else
-    echo "Unsupported Linux distribution"
+    echo "Unsupported Linux"
     exit
   fi
 else
@@ -30,6 +34,7 @@ else
 fi
 
 # Start the Nginx service
+
 if [ "$(uname)" == "Darwin" ]; then
   brew services start nginx
 else
